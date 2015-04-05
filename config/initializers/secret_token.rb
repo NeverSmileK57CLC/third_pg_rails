51f5b4eb8f8297e8903202b61d4ab00ca9b2e52c
NeverSmileK57CLC <neversmile12b1@gmail.com>
2015-04-05 13:39:47 +0700

    message

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ThirdRails::Application.config.secret_key_base = 'ab9b2efdc092fc261861625a022a4c5c91c952d66e778d065d992d2f85c1aa42f6ad6d1d5271b7b66f8d8d3a0bb0409941ea1ac6deb0c6a8180ce74c8dae70da'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ThirdRails::Application.config.session_store :cookie_store, key: '_third_rails_session'
